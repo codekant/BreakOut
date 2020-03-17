51b661dc0882e63be3f8d3ea346cb1c4505171d1
Glitch (-breakout-) <none>
2020-03-17 07:07:19 +0000

    We love it with more coordinates

--- Coordination/Mapping.js
+++ Coordination/Mapping.js
@@ -3,78 +3,162 @@ module.exports = [
     name: "Train Station",
     id: "trainstation",
     description:
-      "Go here and wait for the train to pass by, as it does get in it and rob it!"
+      "Go here and wait for the train to pass by, as it does get in it and rob it!",
+    distance: 2000,
+    cd: {
+      x: 200,
+      y: 100
+    }
   },
   {
     name: "Apartment",
     id: "appartment",
     description:
-      "Stay here for the rest of your BreakOut day, take rest and stay safe. You will be given a flat for cost. Protects you from your opponent and doesn't allow them to kill you."
+      "Stay here for the rest of your BreakOut day, take rest and stay safe. You will be given a flat for cost. Protects you from your opponent and doesn't allow them to kill you.",
+    distance: 2300,
+    cd: {
+      x: 544,
+      y: 193
+    }
   },
   {
     name: "Jewel Store",
     id: "jewellery",
     description:
-      "Open at times, go here and rob the jewels and then go to the crime base and deposit to get your cash!"
+      "Open at times, go here and rob the jewels and then go to the crime base and deposit to get your cash!",
+    distance: 2400,
+    cd: {
+      x: 705,
+      y: 192
+    }
   },
   {
     name: "Crime Base",
     id: "crimebase",
-    description: "You may spawn here when you get killed from anything other than police, get guns and cars. Come here when you find a airdrop lootbox, to deposit and get the money"
+    description:
+      "You may spawn here when you get killed from anything other than police, get guns and cars. Come here when you find a airdrop lootbox, to deposit and get the money",
+    distance: 3000,
+    cd: {
+      x: 1062,
+      y: 132
+    }
   },
   {
     name: "Doughnut Store",
     id: "doughnutshop",
-    description: "Get yourself a doughnut here!, doughnuts are helpfull as they regain 50 health when you use them. Also rob this place when you have nothing else to do, you have chances of getting 750$"
+    description:
+      "Get yourself a doughnut here!, doughnuts are helpfull as they regain 50 health when you use them. Also rob this place when you have nothing else to do, you have chances of getting 750$",
+    distance: 2500,
+    cd: {
+      x: 996,
+      y: 193
+    }
   },
   {
     name: "Bank",
     id: "bank",
-    description: "Mystery to go inside, you need a keycard which you get from pitpocketing a police to go inside a bank, rob it and get 1500"
+    description:
+      "Mystery to go inside, you need a keycard which you get from pitpocketing a police to go inside a bank, rob it and get 1500",
+    distance: 1200,
+    cd: {
+      x: 360,
+      y: 197
+    }
   },
   {
     name: "Garage",
     id: "garage",
-    description: "Get yourself a car or vehicle here!"
+    description: "Get yourself a car or vehicle here!",
+    distance: 2000,
+    cd: {
+      x: 315,
+      y: 382
+    }
   },
   {
     name: "Gas Station",
     id: "gas",
-    description: "Come to get your car fully fueled, also rob here at times when you have to do nothing. You may get 750$ lol."
+    description:
+      "Come to get your car fully fueled, also rob here at times when you have to do nothing. You may get 750$ lol.",
+    distance: 800,
+    cd: {
+      x: 170,
+      y: 387
+    }
   },
   {
     name: "Golf Arena",
     id: "golf",
-    description: "Come to play Golf here! Play Tournaments and win cash!"
+    description: "Come to play Golf here! Play Tournaments and win cash!",
+    distance: 2400,
+    cd: {
+      x: 515,
+      y: 364
+    }
   },
   {
     name: "Volcano Base",
     id: "volcano",
-    description: "Volcano Base, the richest secret base of the criminals. Rob the museum and power plant and come here to deposit all the loot."
+    description:
+      "Volcano Base, the richest secret base of the criminals. Rob the museum and power plant and come here to deposit all the loot.",
+    distance: 2920,
+    cd: {
+      x: 518,
+      y: 384
+    }
   },
   {
     name: "Museum",
     id: "museum",
-    description: "Here come rob all the dinosaur bones, and other important articles here. They cost high, go to the volcano base to redeem the cash out of these articles"
+    description:
+      "Here come rob all the dinosaur bones, and other important articles here. They cost high, go to the volcano base to redeem the cash out of these articles",
+    distance: 2200,
+    cd: {
+      x: 1054,
+      y: 493
+    }
   },
   {
     name: "Area 51",
     id: "area51",
-    description: "Don't worry criminals, one day you will be here. One of the biggest criminals are kept here, secret plans on raids from the cops are made here. any criminal who comes here without keycard is shot to death. No Questions. only the criminals arrested when the had over 3k bounty are kept here escaping here needs you to have over 250 energy"
+    description:
+      "Don't worry criminals, one day you will be here. One of the biggest criminals are kept here, secret plans on raids from the cops are made here. any criminal who comes here without keycard is shot to death. No Questions. only the criminals arrested when the had over 3k bounty are kept here escaping here needs you to have over 250 energy",
+    distance: 1600,
+    cd: {
+      x: 770,
+      y: 658
+    }
   },
   {
     name: "Prison",
     id: "prison",
-    description: "Criminals or Cops, may it be anyone. either spawn here, except for sometimes when criminals spawn at area 51"
+    description:
+      "Criminals or Cops, may it be anyone. either spawn here, except for sometimes when criminals spawn at area 51",
+    distance: 1000,
+    cd: {
+      x: 318,
+      y: 818
+    }
   },
   {
     name: "PowerPlant",
     id: "powerplant",
-    description: "Here's the power source for the overall energy of all users and the whole city of BreakOut. Come here and rob Uranium, reach the volcano base as soon as possible to deposit all this loot and get money upto $6.25k"
+    description:
+      "Here's the power source for the overall energy of all users and the whole city of BreakOut. Come here and rob Uranium, reach the volcano base as soon as possible to deposit all this loot and get money upto $6.25k",
+    distance: 1600,
+    cd: {
+      x: 866,
+      y: 718
+    }
   },
   {
     name: "Gun Shop",
     id: "gunshop",
-    description: "Get yourself a few guns from here!"
+    description: "Get yourself a few guns from here!",
+    distance: 2000,
+    cd: {
+      x: 1073,
+      y: 718
+    }
   }
 ];
